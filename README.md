468ee2abf178bf18a08869e2dfc58bcae0acbf10
FilimonovEugene <e.filimonov.ext@sportradar.com>
2022-03-29 11:31:16 +0300

    Upload script before running

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ This repo automatically creates a set of a free-tier EC2 instances and start a D
 2. Open variables.tf, update variables 'aws_access_key' and 'aws_secret_key' with the values you created;
 3. Open terminal, navigate to the repo folder;
 4. Execute these commands: "terraform init", "terraform apply";
-5. If you need to re-create instances execute "terraform destroy" and then commands from the previous paragraph.
+5. If you need to re-create instances execute "terraform destroy" and then "terraform apply".
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,20 +1,47 @@
-resource "aws_instance" "fighter" {
-  ami                         = var.ami_id
-  monitoring                  = true
-  instance_type               = var.instance_type
-  key_name                    = var.ssh_key_name
-  associate_public_ip_address = true
-  vpc_security_group_ids      = ["${var.security_group_id}"]
+locals {
+  ssh_key_name = "ssh-key"
+}
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    host        = self.public_ip
-    private_key = "${var.private_key_pem}"
-    timeout     = "10m"
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = local.ssh_key_name
+  public_key = "${tls_private_key.pk.public_key_openssh}"
+}
+
+resource "aws_security_group" "grant_ssh" {
+  name        = "grant_ssh"
+  description = "grant_ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  provisioner "remote-exec" {
-    script = "${path.module}/provisioning.sh"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+module "ec2" {
+  source            = "./modules/ec2"
+  count             = var.instance_count
+  instance_type     = var.instance_type
+  ami_id            = data.aws_ami.ubuntu.id
+  private_key_pem   = tls_private_key.pk.private_key_pem
+  ssh_key_name      = aws_key_pair.kp.key_name
+  security_group_id = aws_security_group.grant_ssh.id
+}
+
+resource "local_file" "private_key" {
+    content  = tls_private_key.pk.private_key_pem
+    filename = "${local.ssh_key_name}.pem"
 }
